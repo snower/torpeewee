52245a3306d1cee6a188adad61ad95758ea91d6c
snower <sujian199@gmail.com>
2017-09-05 17:21:19 +0800

    change version to 0.0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='torpeewee',
-    version='0.0.5',
+    version='0.0.6',
     packages=['torpeewee'],
     install_requires=[
         'tornado>=4.1',
